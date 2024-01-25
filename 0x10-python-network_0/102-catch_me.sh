56f67715be11a8275043ca46c68202d31fa56719
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-25 09:25:34 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This Bash script makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
 
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Use curl to make the request and display the message using the -w option
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" -w "You got me!\n" 0.0.0.0:5000/catch_me
