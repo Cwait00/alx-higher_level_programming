63152988b4812290ae1200a495924b332d32a39c
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-26 09:20:09 -0800

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,10 +7,18 @@ and displays the value of the X-Request-Id variable found in the header of the r
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-
+def fetch_x_request_id(url):
     with urllib.request.urlopen(url) as response:
         # Get the value of the X-Request-Id header from the response
         x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
+        return x_request_id
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    x_request_id = fetch_x_request_id(url)
+    print(x_request_id)
