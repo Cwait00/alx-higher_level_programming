e9ef661af29d8402f34e4e8a659dc1bf70e2dbe9
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-26 11:10:04 -0800

    updated

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
 import requests
 import sys
 
@@ -8,5 +15,4 @@ if __name__ == "__main__":
     payload = {'email': email}
     response = requests.post(url, data=payload)
 
-    print("Your email is:", email)
-    print("Response body:", response.text)
+    print(f"Your email is: {response.text}")
