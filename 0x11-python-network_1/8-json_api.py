e7425b4a8011f41113eda95cc0b865288ba2870e
Cwait00 <cwaitanobongoza@gmail.com>
2024-01-26 11:13:59 -0800

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_response = response.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
