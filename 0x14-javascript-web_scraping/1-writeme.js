25001303d2545955db2d2a48a108efd20b356295
cwaita <cwaitanobongoza@gmail.com>
2024-02-20 04:05:53 -0800

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,20 @@
 
 const fs = require('fs');
 
+// Check if the correct number of arguments are provided
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// Write the string to the file
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
+    process.exit(1);
   }
   console.log(`${content} has been written to ${filePath}`);
 });
