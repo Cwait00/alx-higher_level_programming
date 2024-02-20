650c16c70a93609bb16a863a832b931e72413245
cwaita <cwaitanobongoza@gmail.com>
2024-02-20 06:36:24 -0800

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get movie ID from command line arguments
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Function to fetch characters from the API and print them
+function fetchAndPrintCharacters(movieId) {
+    request(apiUrl, function(error, response, body) {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            const film = JSON.parse(body);
+            console.log(`Characters from ${film.title}:`);
+            film.characters.forEach(character => {
+                request(character, function(error, response, body) {
+                    if (error) {
+                        console.error('Error:', error);
+                    } else {
+                        const characterData = JSON.parse(body);
+                        console.log(characterData.name);
+                    }
+                });
+            });
+        }
+    });
+}
+
+// Call the function to fetch and print characters
+fetchAndPrintCharacters(movieId);
