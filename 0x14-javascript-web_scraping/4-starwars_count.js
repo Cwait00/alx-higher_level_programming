811d000c747e2f4e046d05cfb985e3a648d68d32
cwaita <cwaitanobongoza@gmail.com>
2024-02-21 03:56:02 -0800

    updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,17 +5,17 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = '18'; // Wedge Antilles character ID
 
-request(apiUrl, function(error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const films = JSON.parse(body).results;
-        const count = films.reduce((total, film) => {
-            if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-                return total + 1;
-            }
-            return total;
-        }, 0);
-        console.log(count);
-    }
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const count = films.reduce((total, film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        return total + 1;
+      }
+      return total;
+    }, 0);
+    console.log(count);
+  }
 });
