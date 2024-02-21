3764ff711faa719ec70e73f67c64d93d4dca9487
cwaita <cwaitanobongoza@gmail.com>
2024-02-21 03:50:18 -0800

    updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,12 +7,12 @@ const movieID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Unexpected status code:', response.statusCode);
-    } else {
-        const movieData = JSON.parse(body);
-        console.log(movieData.title);
-    }
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
 });
