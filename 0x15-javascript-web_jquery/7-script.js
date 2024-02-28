d689cbcb089e8f64636e1fabc8fdd7f3d8415e26
cwaita <cwaitanobongoza@gmail.com>
2024-02-28 05:28:52 -0800

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+// Ensure the script is executed only after the DOM has loaded
+$(document).ready(function() {
+    // Make a GET request to fetch character data from the URL
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
+        // Extract the character name from the response data
+        var characterName = data.name;
+
+        // Select the DIV#character element using the jQuery API and update its content with the character name
+        $('#character').text(characterName);
+    });
+});
