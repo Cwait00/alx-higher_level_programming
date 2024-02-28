109150d87725921fb4fd0130a3ee5dc17b1269a7
cwaita <cwaitanobongoza@gmail.com>
2024-02-28 06:01:28 -0800

    8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,13 @@
 // Ensure the script is executed only after the DOM has loaded
 $(document).ready(function() {
     // Make a GET request to fetch movie data from the URL
-    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json',
+	    function(data) {
         // Extract movie titles from the response data
         var movies = data.results;
 
-        // Iterate over the movie titles and append them to the UL#list_movies element
+        // Iterate over the movie titles and append them
+	    // to the UL#list_movies element
         $.each(movies, function(index, movie) {
             // Create a new <li> element for each movie title
             var listItem = $('<li></li>').text(movie.title);
