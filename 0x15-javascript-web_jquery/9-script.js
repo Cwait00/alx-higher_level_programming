83811a52fa13c335d281af695b187e4f7ab79998
cwaita <cwaitanobongoza@gmail.com>
2024-02-28 06:02:44 -0800

    9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -3,6 +3,7 @@ $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function(data) {
     // Extract the translation of "hello" from the response data
     var helloTranslation = data.hello;
 
-    // Select the DIV#hello element using the jQuery API and update its content with the translation
+    // Select the DIV#hello element using the jQuery API and
+	// update its content with the translation
     $('#hello').text(helloTranslation);
 });
